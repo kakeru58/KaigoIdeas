0cfe1df1c7c878a61f51a7ea4265e28f2d60f108
kakeru58 <yamasaki586868@gmail.com>
2024-07-10 23:19:32 +0900

    fix:ロゴをhtmlに

--- app.py
+++ app.py
@@ -143,5 +143,12 @@ with footer_col1:
     st.write("© 2024 Kakeru Yamasaki. Licensed under the MIT License.")
 
 with footer_col2:
-    logo_footer = Image.open('logo1.png')
-    st.image(logo_footer, width=50)
+    st.markdown(
+        """
+        <div style="display: flex; align-items: center;">
+            <span>© 2024 Kakeru Yamasaki. Licensed under the MIT License.</span>
+            <img src="logo1.png" alt="Logo" style="width: 50px; margin-left: 10px;">
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
